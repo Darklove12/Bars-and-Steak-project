999b9182f6b7fc6bb9808e4dbaa5e6afee90ac6a
Bathande <bsinukela17@gmail.com>
2024-05-08 09:07:58 +0200

    added another image

--- Steak/src/Page/AboutUs.jsx
+++ Steak/src/Page/AboutUs.jsx
@@ -24,11 +24,11 @@ const AboutUs = () => {
              </div>
 
          <div>
-                 {/* <img
+                 <img
                      src="https://images.unsplash.com/photo-1474898856510-884a2c0be546?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
                      alt=""
                  className="w-[49.9%] h-[50rem] mb-[1rem] shadow-md"
-             /> */}
+             />
                 </div>
 
                 {/* <div>
